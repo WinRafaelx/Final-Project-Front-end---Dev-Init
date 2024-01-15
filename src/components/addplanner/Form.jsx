c49071e8768b8cb6855a23250fb25b6f74f9fd73
MawinS <meow7747@gmail.com>
2024-01-15 16:34:29 +0700

    feat: add some checkbox for planner to add more than one task

--- src/components/addplanner/Form.jsx
+++ src/components/addplanner/Form.jsx
@@ -1,10 +1,11 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPlanner, addEvent } from "../../libs/PlannerSlice";
 import { months } from "../../models/model";
 import { useNavigate, useLocation, useParams } from "react-router-dom";
 
 const Form = () => {
+  const [addOneMore, setAddOneMore] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { dateVal } = useParams();
@@ -24,15 +25,15 @@ const Form = () => {
   }, [queryDate]);
 
   const handleDateFormat = (date) => {
-    let [day, month, year] = date.split("-")
+    let [day, month, year] = date.split("-");
     if (day.length === 1) {
-      day = "0" + day
+      day = "0" + day;
     }
     if (month.length === 1) {
-      month = "0" + month
+      month = "0" + month;
     }
 
-    return `${year}-${month}-${day}`
+    return `${year}-${month}-${day}`;
   };
 
   const handleSubmit = (e) => {
@@ -42,9 +43,10 @@ const Form = () => {
     const dateValue = dateRef.current.value;
     const taskValue = taskRef.current.value;
 
-    const [year, month, date] = dateValue.split("-").map(part => parseInt(part, 10));
+    const [year, month, date] = dateValue
+      .split("-")
+      .map((part) => parseInt(part, 10));
 
-    dateRef.current.value = "";
     taskRef.current.value = "";
 
     dispatch(
@@ -55,9 +57,10 @@ const Form = () => {
         task: taskValue,
       })
     );
-    navigate("/planner");
+    if (!addOneMore) {
+      navigate("/planner");
+    }
   };
-
 
   return (
     <form className="max-w-sm mx-auto mt-10" onSubmit={handleSubmit}>
@@ -87,12 +90,39 @@ const Form = () => {
           required
         />
       </div>
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Submit
-      </button>
+      <div className="inline-flex items-center mb-3">
+      <label className="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="check">
+        <input
+          type="checkbox"
+          className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+          id="check"
+          onClick={() => setAddOneMore(!addOneMore)}
+        />
+        <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" strokeWidth="1">
+            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
+          </svg>
+        </span>
+      </label>
+      <label className="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="check">
+        Add more than one task
+      </label>
+    </div>
+      <div className="flex justify-between">
+        <button
+          type="submit"
+          className="mr-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm flex-grow px-5 py-2.5 text-center"
+        >
+          Submit
+        </button>
+        <button
+          type="button"
+          onClick={() => navigate("/planner")}
+          className="ml-2 text-white bg-rose-500 hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm flex-grow px-5 py-2.5 text-center"
+        >
+          Back
+        </button>
+      </div>
     </form>
   );
 };
--- src/components/planner/DayCell.jsx
+++ src/components/planner/DayCell.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ModalComponent from "./ModalComponent";
+import { today } from "../../models/model";
 
 const DayCell = ({ day, thismonth, events }) => {
   const [showModal, setShowModal] = useState(false);
@@ -16,7 +17,7 @@ const DayCell = ({ day, thismonth, events }) => {
     <>
       <td
         className={`border p-1 h-40 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 overflow-auto transition cursor-pointer duration-500 ease hover:bg-gray-400
-        ${thismonth ? "bg-white" : "bg-slate-300"}`}
+        ${thismonth && today.getDate() === day.getDate() ? "bg-sky-200" : thismonth ? "bg-white" : "bg-slate-300"}`}
         onClick={openModal}
       >
         <div className="flex flex-col h-40 mx-auto xl:w-40 lg:w-30 md:w-30 sm:w-full w-10 mx-auto overflow-hidden relative">
@@ -25,17 +26,24 @@ const DayCell = ({ day, thismonth, events }) => {
           </div>
           <div className="bottom flex-grow h-30 py-1 w-full cursor-pointer hidden sm:block">
             {/* Render events or other content for the day */}
-            {events.map((event, index) => (
+            {events.length <=3 ? events.map((event, index) => (
               <div
                 key={index}
-                className="text-white bg-orange-400 rounded p-1 m-1"
+                className="text-white bg-blue-400 rounded py-1 px-2 m-1"
               >
                 {event}
               </div>
-            ))}
+            )) : (
+              <div
+                className="text-white bg-blue-400 rounded py-1 px-2 m-1"
+              >
+                <div>{events.length} events </div>
+                <div>䷮ Press to View</div>
+              </div>
+            )}
           </div>
           <span
-            className={`absolute bottom-0 right-0 p-1 text-white bg-red-500 rounded-full block sm:hidden flex items-center justify-center
+            className={`absolute bottom-0 right-0 p-1 text-white bg-rose-500 rounded-full block sm:hidden flex items-center justify-center
               ${thismonth && events.length > 0 ? "" : "hidden"}`}
             style={{ width: "30px", height: "30px" }}
           >
