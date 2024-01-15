9a934c8e7bfe77933c85bb97a7ea5075ffba5db1
MawinS <meow7747@gmail.com>
2024-01-15 20:35:04 +0700

    feat: rename website

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
             alt="Flowbite Logo"
           /> */}
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Routines
+            🍀 LifeLogHub
           </span>
         </Link>
         <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
